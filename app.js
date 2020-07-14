93f8114992a6b81631d16283961264c9076579dc
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-14 16:55:30 +0900

    updated.

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ var word = document.getElementById('qwerty');
 var phrase = document.getElementById('phrase');
 var missed = 0;
 
+var buttonElement = document.getElementsByClassName('btn__reset')[0];
+console.log(buttonElement);
+
+buttonElement.addEventListener('click', () => {
+  const overlay = document.getElementById('overlay');
+  overlay.style.display = 'none';
+});
