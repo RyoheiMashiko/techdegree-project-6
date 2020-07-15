5d3f6d6ccbf0a96fddc2e6cda1feb1e3403ab49c
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-07-15 14:08:25 +0900

    Update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var word = document.getElementById('qwerty');
-var phrase = document.getElementById('phrase');
+var inputPhrase = document.getElementById('phrase');
 var missed = 0;
 
 var buttonElement = document.getElementsByClassName('btn__reset')[0];
@@ -9,3 +9,10 @@ buttonElement.addEventListener('click', () => {
   const overlay = document.getElementById('overlay');
   overlay.style.display = 'none';
 });
+
+const phrases = ['country', 'dish', 'animal', 'nature', 'bedding'];
+
+function getRandomPhraseAsArray(arr){
+  let split = phrases[Math.floor(Math.random() * phrases.length)] ;
+  return split;
+} 
